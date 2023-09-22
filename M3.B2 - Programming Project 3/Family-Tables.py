34de9c9fa717d596f8266749a8c987a2205057d0
sophiadicuffa <sophiadicuffa@gmail.com>
2023-09-21 21:52:25 -0400

    process line

--- M3.B2 - Programming Project 3/Family-Tables.py
+++ M3.B2 - Programming Project 3/Family-Tables.py
@@ -1,11 +1,98 @@
-# Nadeya DeDiago, Sophia DiCuffa, Amanda Vu - CS555 M3.B2
+# Sophia DiCuffa, Nadeya DeDiago, Amanda Vu
+# CS555 M3.B2
 
-# Reads each line of the GEDCOM file
+from datetime import datetime
+
+people = []
+families = []
+current_person = {}
+current_family = {}
+
+# Function to calculate age
+def calculate_age(birthdate, deathdate):
+    if not birthdate:
+        return "N/A"
+    
+    birthdate = datetime.strptime(birthdate, "%d %b %Y")
+    today = datetime.today()
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    
+    if deathdate:
+        deathdate = datetime.strptime(deathdate, "%d %b %Y")
+        age_at_death = deathdate.year - birthdate.year - ((deathdate.month, deathdate.day) < (birthdate.month, birthdate.day))
+        return f"{age} (Died at {age_at_death})"
+    
+    return age
+
+# Read GEDCOM file
 def process_line(line):
-    line = line.strip()
-    parts = line.split(' ', 2)
 
-# Read the GEDCOM file
-with open('My-Family.ged', 'r') as gedcom_file:
+    global current_person
+    global current_family
+
+    line = line.strip()
+    parts = line.split()
+    level = int(parts[0]) # 0, 1 ,2 
+    tag = parts[1] # INDI, FAM, etc.
+
+    if level == 0:
+        if tag == 'INDI':
+            if current_person:
+                people.append(current_person)
+            current_person = {}  # Initialize a new person dictionary
+        elif tag == 'FAM':
+            if current_family:
+                families.append(current_family)
+            current_family = {}
+    elif level == 1:
+        if tag == 'NAME':
+            current_person['NAME'] = ' '.join(parts[2:])
+            if current_person not in people:
+               people.append(current_person)
+        elif tag == 'SEX':
+            current_person['SEX'] = parts[2]
+        elif tag == 'BIRT':
+            current_person['BIRTH'] = {}
+        elif tag == 'FAMS':
+            current_person.setdefault('FAMS', []).append(parts[2])
+        elif tag == 'FAMC':
+            current_person['FAMC'] = parts[2]
+        elif tag == 'DEAT':
+            current_person['DEATH'] = {}
+        elif tag == 'DATE':
+            if 'BIRTH' in current_person:
+                current_person['BIRTH']['DATE'] = ' '.join(parts[2:])
+            elif 'DEATH' in current_person:
+                current_person['DEATH']['DATE'] = ' '.join(parts[2:])
+    elif level == 2:
+        if 'BIRTH' in current_person:
+            if tag == 'DATE':
+                current_person['BIRTH']['DATE'] = ' '.join(parts[2:])
+        elif 'MARR' in current_family:
+            if tag == 'DATE':
+                current_family['MARR']['DATE'] = ' '.join(parts[2:])
+    
+
+# open GEDCOM file
+with open('test.ged', 'r') as gedcom_file:
     for line in gedcom_file:
         process_line(line)
+
+#if current_person not in people:
+ #   people.append(current_person) 
+
+
+# table to print individuals 
+print("people:")
+print("{:<10} {:<30} {:<10} {:<15} {:<20} {:<10}".format("ID", "Name", "Sex", "Birthday", "Age", "Status"))
+for person in people:
+    indi_id = person.get('INDI', '')
+    name = person.get('NAME', '')
+    sex = person.get('SEX', '')
+    birthday = person.get('BIRTH', {}).get('DATE', '')
+    death_date = person.get('DEATH', {}).get('DATE', '')
+    age = calculate_age(birthday, death_date)
+    status = "Alive" if not death_date else "Dead"
+    print("{:<10} {:<30} {:<10} {:<15} {:<20} {:<10}".format(indi_id, name, sex, birthday, age, status))
+
+
