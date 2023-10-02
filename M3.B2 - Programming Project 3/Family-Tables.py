657ca7ce327d8c8ea232176639a6600f957f507e
amandavu <66039919+amandavu@users.noreply.github.com>
2023-10-01 20:45:33 -0400

    Update Family-Tables.py

--- M3.B2 - Programming Project 3/Family-Tables.py
+++ M3.B2 - Programming Project 3/Family-Tables.py
@@ -246,3 +246,21 @@ def check_marriage_before_death(people, families):
     return True
 
 check_marriage_before_death(people, families)
+
+def is_birth_before_death(individual):
+    birth_date = individual.get("birth_date")
+    death_date = individual.get("death_date")
+    
+    if birth_date and death_date:
+        birth_date = datetime.strptime(birth_date, "%Y-%m-%d")
+        death_date = datetime.strptime(death_date, "%Y-%m-%d")
+        
+        if birth_date <= death_date:
+            return True
+        else:
+            error_message = f"ERROR: INDIVIDUAL: US03: {individual.get('INDI', '')}: Died {death_date} before born {birth_date}"
+            print(error_message)
+            return False
+    
+    return False
+
