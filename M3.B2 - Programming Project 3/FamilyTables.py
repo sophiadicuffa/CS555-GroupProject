7fea6b291512edbcc25553f18b93bfe28f1b274c
amandavu <66039919+amandavu@users.noreply.github.com>
2023-10-16 18:55:36 -0400

    Update FamilyTables.py

--- M3.B2 - Programming Project 3/FamilyTables.py
+++ M3.B2 - Programming Project 3/FamilyTables.py
@@ -463,43 +463,6 @@ def check_birth_before_parents_marriage(people, families):
 
 check_birth_before_parents_marriage(people, families)
 
-def check_birth_before_death_of_parents(people, families):
-    for family in families:
-        children = family.get("CHIL", [])
-        husband_id = family.get("HUSB", "")
-        wife_id = family.get("WIFE", "")
-        
-        for child_id in children:
-            child = next((person for person in people if person.get('INDI', '') == child_id), None)
-            mother = next((person for person in people if person.get('INDI', '') == wife_id), None)
-            father = next((person for person in people if person.get('INDI', '') == husband_id), None)
-            print(child)
-
-            if child and mother and father:
-                
-                child_birth_date = child.get("BIRT", {}).get("BDATE", "")
-                mother_death_date = mother.get("DEAT", {}).get("DATE", "")
-                father_death_date = father.get("DEAT", {}).get("DATE", "")
-
-                if child_birth_date and mother_death_date:
-                    child_birth_date_format = datetime.strptime(child_birth_date, "%d %b %Y")
-                    mother_death_date_format = datetime.strptime(mother_death_date, "%d %b %Y")
-                    
-
-                    if child_birth_date_format > mother_death_date_format:
-                        error_message = f"ERROR: INDIVIDUAL: US09: {child_id}: Child born {child_birth_date_format.strftime('%d %b %Y')} after mother's death {mother_death_date_format.strftime('%d %b %Y')}"
-                        print(error_message)
-
-                if child_birth_date and father_death_date:
-                    child_birth_date_format = datetime.strptime(child_birth_date, "%d %b %Y")
-                    father_death_date_format = datetime.strptime(father_death_date, "%d %b %Y")
-
-                    if child_birth_date_format > father_death_date_format:
-                        error_message = f"ERROR: INDIVIDUAL: US09: {child_id}: Child born {child_birth_date_format.strftime('%d %b %Y')} after father's death {father_death_date_format.strftime('%d %b %Y')}"
-                        print(error_message)
-
-check_birth_before_death_of_parents(people, families)
-
 def check_male_last_names(people, families):
     for family in families:
         husband_id = family.get('HUSB', '')
